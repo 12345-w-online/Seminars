1f35f5afb512b70bfa97561ea00a270f76f85f04
ShestakovIS <12345-w-online@list.ru>
2023-10-03 21:53:12 +0300

    'tasks for week 4 - graphics'

--- main.py
+++ main.py
@@ -1,5 +1,2 @@
-import fractions
 import numpy as np
-A_inv = np.array([ 0.62087912, -0.42307692,  0.24725275])
-np.set_printoptions(formatter={'all':lambda x: str(fractions.Fraction(x).limit_denominator())})
-print(A_inv)
+print(round(np.linalg.det([[a, b, c], [-5, 0, 3], [3, -4, -4]])))   
